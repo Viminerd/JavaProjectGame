e4fa6eba0d5daa843ce3766248f8a27a6b85aca4
Viminerd <viktormineur@gmail.com>
2023-02-23 10:57:33 +0100

    Package movage

--- src/GamePackage/ProjectMain.java
+++ src/GamePackage/ProjectMain.java
@@ -1,13 +1,11 @@
-
-
-public class ProjectMain {
+package GamePackage;
+public class ProjectMain{
 
 	public ProjectMain() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
