1170dfb1047cb3e85ea142e6a0d42780f227bb63
osclo923 <osclo923@tlvm-4-1-3.ad.liu.se>
2023-02-23 11:29:07 +0100

    Entity

--- src/Objects/Entity.java
+++ src/Objects/Entity.java
@@ -1,12 +1,20 @@
+/**
+ * Entity graphics and movement defined. 
+ */
+
 package Objects;
 
-import javafx.scene.canvas.GraphicsContext;
 
-public class Entity {
-	
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Shape;
+
+public class Entity extends Canvas {
+	protected GraphicsContext gc =  getGraphicsContext2D();
+	protected Shape shape;
 
 	public Entity() {
-		GraphicsContext
+	
 		
 		// TODO Auto-generated constructor stub
 	}
