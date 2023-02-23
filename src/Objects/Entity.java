e94d9e7299b02ca30d561c63dd651c52d05c4743
osclo923 <osclo923@tlvm-4-1-3.ad.liu.se>
2023-02-23 10:51:04 +0100

    Everything

--- src/Objects/Entity.java
+++ src/Objects/Entity.java
@@ -1,8 +1,13 @@
 package Objects;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Entity {
+	
 
 	public Entity() {
+		GraphicsContext
+		
 		// TODO Auto-generated constructor stub
 	}
 
