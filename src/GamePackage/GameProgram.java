efa216dcd2769c28e9dd0a0e69dde363786b5c92
osclo923 <osclo923@su17-107.ad.liu.se>
2023-02-24 11:30:28 +0100

    Separated and structured Gameprogram and ProjectMain to work together properly

--- src/GamePackage/GameProgram.java
+++ src/GamePackage/GameProgram.java
@@ -1,10 +1,8 @@
 package GamePackage;
 
 public class GameProgram {
-
 	public GameProgram(String difficulty) {
-		
-		
+		Scene.EasyMap map = new Scene.EasyMap(ProjectMain.getGameCanvas());
 	}
 	
 	public void EasyMap() {
--- src/GamePackage/ProjectMain.java
+++ src/GamePackage/ProjectMain.java
@@ -16,7 +16,11 @@ import javafx.stage.Stage;
 
 public class ProjectMain extends Application {
 	public static Canvas gameCanvas = new Canvas(1200, 600);
-	private GraphicsContext g = gameCanvas.getGraphicsContext2D();
+	private GraphicsContext gc = gameCanvas.getGraphicsContext2D();
+
+	public static Canvas getGameCanvas() {
+		return gameCanvas;
+	}
 
 	public static void main(String[] args) {
 		launch(args);
@@ -26,25 +30,20 @@ public class ProjectMain extends Application {
 		primaryStage.setTitle("Oscar och Viktors Canvas!");
 
 		BorderPane mainlayout = new BorderPane();
-		g.setFill(Color.LIGHTGREEN);
-		g.fillRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
+		gc.setFill(Color.LIGHTGREEN);
+		gc.fillRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
 
 		mainlayout.setCenter(gameCanvas);
 		Scene mainScene = new Scene(mainlayout);
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
-		easyMap();
-	}
-
-	public void runGame() { //runs when start in main meny has been pressed
-		new GameProgram();
-		new Player(10, 10, 10);
-	}
-
-	public void easyMap() {
-		new EasyMap(gameCanvas);
+		runGame("easy"); //test
 		
+	}
 
+	public void runGame(String difficulty) { // runs when start in main meny has been pressed
+		GameProgram newGame=new GameProgram(difficulty);
+		Player player = new Player(10, 10, 10);
 	}
 
 }
