39791945496d5f2fcb4cd88b6e16b1d5d92b0b00
Viminerd <viktormineur@gmail.com>
2023-02-24 12:07:14 +0100

    24/2 changes

--- src/GamePackage/GameProgram.java
+++ src/GamePackage/GameProgram.java
@@ -2,7 +2,7 @@ package GamePackage;
 
 public class GameProgram {
 	public GameProgram(String difficulty) {
-		Scene.EasyMap map = new Scene.EasyMap(ProjectMain.getGameCanvas());
+		Scene.EasyMap map = new Scene.EasyMap(ProjectMain.gameCanvas);
 	}
 	
 	public void EasyMap() {
