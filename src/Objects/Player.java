e1d2de0b0393f554f2e5e8ae5f33e9b6ed566fe0
osclo923 <osclo923@su17-107.ad.liu.se>
2023-03-02 15:31:09 +0100

    Began working on healthbar

--- src/Objects/Player.java
+++ src/Objects/Player.java
@@ -28,9 +28,14 @@ public class Player extends Character {
 	private Shape aimRay;
 	private double aimx;
 	private double aimy;
+	private Rectangle healthBar; 
+	private Rectangle missingHealthBar; 
+	private double healthScale;
+	private double maxHealth;
 
 	public Player(double posx, double posy, int health) {
 		super(posx, posy, health);
+		maxHealth=health;
 		moveSpeed = 3;
 		hitBox = new Circle(posx, posy, 15);
 		hitBox.setLayoutX(posx);
@@ -39,6 +44,17 @@ public class Player extends Character {
 		hitBox.setStroke(Color.GREEN);
 		ProjectMain.mainlayout.getChildren().add(hitBox);
 		GameProgram.entityList.add(this);
+		
+		
+		healthBar = new Rectangle(posx, posy, 10 , 10);
+		healthBar.setFill(Color.GREEN);
+		ProjectMain.mainlayout.getChildren().add(this.healthBar);
+		
+
+
+		missingHealthBar = new Rectangle(posx+8-35+70*healthScale, posy-17, 0 , 10);
+		missingHealthBar.setFill(Color.RED);
+		ProjectMain.mainlayout.getChildren().add(missingHealthBar);
 
 		ProjectMain.mainScene.setOnKeyPressed(e -> {
 			switch (e.getCode()) {
@@ -60,16 +76,14 @@ public class Player extends Character {
 
 		});
 
+		// Dessa måste ligga i en move-funktion för att inte kunna användas när spelet
+		// är pausat.
 		
-		
-		
-		//Dessa måste ligga i en move-funktion för att inte kunna användas när spelet är pausat.
 		ProjectMain.mainScene.setOnKeyReleased(e -> {
 			switch (e.getCode()) {
 			case W:
 				if (newY == -1) {
 					newY += 1;
-
 				}
 				break;
 			case S:
@@ -90,45 +104,35 @@ public class Player extends Character {
 			default:
 				break;
 			}
-
 		});
-
 		ProjectMain.mainScene.setOnMouseDragged(e -> {
-
-			if (e.isSecondaryButtonDown()) {
+			if (e.isSecondaryButtonDown() && GameProgram.gameRunning) {
 				aim(e.getX(), e.getY());
 				secondaryMousePressed = true;
 			}
-			if (e.isPrimaryButtonDown()) {
+			if (e.isPrimaryButtonDown()&& GameProgram.gameRunning) {
 				primaryMousePressed = true;
 			}
-	
 		});
-		
 		ProjectMain.mainScene.setOnMousePressed(e -> {
-
-			if (e.isSecondaryButtonDown()) {
+			if (e.isSecondaryButtonDown()&& GameProgram.gameRunning) {
 				aim(e.getX(), e.getY());
 				secondaryMousePressed = true;
 			}
-				
-			if (e.isPrimaryButtonDown()) {
+			if (e.isPrimaryButtonDown()&& GameProgram.gameRunning) {
 				primaryMousePressed = true;
-				shoot(e.getX(),e.getY());
+				shoot(e.getX(), e.getY());
 			}
 		});
-
 		ProjectMain.mainScene.setOnMouseReleased(e -> {
-
-			if (e.isSecondaryButtonDown() == false) {
+			if (e.isSecondaryButtonDown() == false && GameProgram.gameRunning) {
 				secondaryMousePressed = false;
 				ProjectMain.mainlayout.getChildren().remove(aimRay);
 			}
-			if (e.isPrimaryButtonDown() == false) {
+			if (e.isPrimaryButtonDown() == false&& GameProgram.gameRunning ) {
 				primaryMousePressed = false;
 			}
 		});
-
 	}
 
 	public void move(double newX, double newY) {
@@ -136,13 +140,15 @@ public class Player extends Character {
 		boolean hitsAWallY = false;
 		double moveSpeed = 3;
 		isAiming();
+		updateHealthBar();
 		if (newX != 0 || newY != 0) {
 			if (newX != 0 && newY != 0) {
 				moveSpeed = Math.sqrt(moveSpeed * 2);
 			}
 			hitBox.setLayoutX(getPosx() + newX * moveSpeed);
 			for (Entity entity : GameProgram.entityList) {
-				if (hitBox.getBoundsInParent().intersects(entity.getHitBox().getBoundsInParent()) && entity.getHitBox() != hitBox) {
+				if (hitBox.getBoundsInParent().intersects(entity.getHitBox().getBoundsInParent())
+						&& entity.getHitBox() != hitBox) {
 					hitsAWallX = true;
 					break;
 				}
@@ -151,7 +157,8 @@ public class Player extends Character {
 
 			hitBox.setLayoutY(getPosy() + newY * moveSpeed);
 			for (Entity entity : GameProgram.entityList) {
-				if (hitBox.getBoundsInParent().intersects(entity.getHitBox().getBoundsInParent()) && entity.getHitBox() != hitBox) {
+				if (hitBox.getBoundsInParent().intersects(entity.getHitBox().getBoundsInParent())
+						&& entity.getHitBox() != hitBox) {
 					hitsAWallY = true;
 					break;
 				}
@@ -169,13 +176,14 @@ public class Player extends Character {
 
 	private void shoot(double x, double y) {
 		System.out.println("Pew Pew");
-		
-		Point2D point1 = new Point2D(getPosx()+15, getPosy());
-		Point2D point2 = new Point2D(x,y);
+
+		Point2D point1 = new Point2D(getPosx() + 15, getPosy());
+		Point2D point2 = new Point2D(x, y);
 		Point2D vector = point2.subtract(point1);
 		double angleRad = Math.atan2(vector.getY(), vector.getX());
 		double angle = Math.toDegrees(angleRad);
-		new FriendlyBullet(posx,posy,angle,10);   //ändra rotation här "0", till vinkeln baserad på eventX och eventY. Ex i ranged.
+		new FriendlyBullet(posx, posy, angle, 10); // ändra rotation här "0", till vinkeln baserad på eventX och eventY.
+													// Ex i ranged.
 
 	}
 
@@ -183,7 +191,7 @@ public class Player extends Character {
 		aimx = x;
 		aimy = y;
 		ProjectMain.mainlayout.getChildren().remove(aimRay);
-		aimRay = new Line(getPosx()+10, getPosy()+10, x, y);
+		aimRay = new Line(getPosx() + 10, getPosy() + 10, x, y);
 		aimRay.setStroke(Color.RED);
 		ProjectMain.mainlayout.getChildren().add(aimRay);
 	}
@@ -202,15 +210,30 @@ public class Player extends Character {
 	public void moveMe() {
 		move(newX, newY);
 	}
+	
+	private void updateHealthBar() {
+		healthScale = health/maxHealth;
+//		ProjectMain.mainlayout.getChildren().remove(healthBar);
+//		ProjectMain.mainlayout.getChildren().remove(missingHealthBar);
+		healthBar.setWidth(70*healthScale);
+		healthBar.setLayoutX(posx+8-35);
+		healthBar.setLayoutY(posy-17);
+//		healthBar = new Rectangle(posx+8-35,posy-17 ,70*healthScale,10);
+		healthBar.setFill(Color.GREEN);
+		
+		missingHealthBar.setFill(Color.RED);
+		missingHealthBar.setWidth(health);
+
+	}
 
 	@Override
 	public void damage(double damage) {
-		health -=damage;
-		if (health <1) {
+		health -= damage;
+		if (health < 1) {
 			System.out.println("Player dead");
-			GameProgram.gameRunning = false; 
+			GameProgram.gameRunning = false;
 		}
-		
+
 	}
 
 }
